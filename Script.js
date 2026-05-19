2358790435346bd9c8c5ad4f9709c050c01016f0
mugdha-sarker81 <mugdhasarker81@gmail.com>
2026-05-20 01:39:35 +0600

    Add toggleStage function to show/hide elements

---
+++ Script.js
@@ -0,0 +1,9 @@
+function toggleStage(id) {
+  const element = document.getElementById(id);
+
+  if (element.style.display === "block") {
+    element.style.display = "none";
+  } else {
+    element.style.display = "block";
+  }
+}
