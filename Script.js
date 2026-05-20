07c1321ed071bc395c9d7a1077f05234b1ceca4f
mugdha-sarker81 <mugdhasarker81@gmail.com>
2026-05-20 14:30:38 +0600

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-function toggleStage(id) {
-  const element = document.getElementById(id);
-
-  if (element.style.display === "block") {
-    element.style.display = "none";
-  } else {
-    element.style.display = "block";
-  }
-}
